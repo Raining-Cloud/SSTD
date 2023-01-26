b1af3693f36664c23d48782af234b539e8b2bf99
Paul Masan <me@paulmasan.com>
2023-01-27 00:00:18 +0100

    fixed include dir and added vector comparison

--- Benchmark/DefaultBench.cpp
+++ Benchmark/DefaultBench.cpp
@@ -1,25 +1,14 @@
 #include <benchmark/benchmark.h>
-#include "Containers/Vector.h"
-
 #include <vector>
 
-static void SSTDVector(benchmark::State& state) {
+#include "Containers/Vector.h"
+
+static void DefaultBench(benchmark::State& state) {
   for (auto _ : state)
   {
-    SSTD::Vector<int> v;
-    benchmark::DoNotOptimize(v);
   }
 }
 
-static void STDVector(benchmark::State& state) {
-  for (auto _ : state)
-  {
-    std::vector<int> v;
-    benchmark::DoNotOptimize(v);
-  }
-}
-
-BENCHMARK(SSTDVector);
-BENCHMARK(STDVector);
+BENCHMARK(DefaultBench);
 
 BENCHMARK_MAIN();
--- Testing/DefaultTest.cpp
+++ Testing/DefaultTest.cpp
@@ -1,6 +1,5 @@
 #include <gtest/gtest.h>
 
-
 // Demonstrate some basic assertions.
 TEST(DefaultTest, BasicAssertions) {
   // Expect two strings not to be equal.
