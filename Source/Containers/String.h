7de05e59b111384eb93c76e8d19857c0f5d55cfb
Paul Masan <me@paulmasan.com>
2023-04-12 22:51:07 +0200

    added string with sso32 implementation

--- Source/Containers/String.h
+++ Source/Containers/String.h
@@ -5,6 +5,9 @@
 #include "General/Memory.h"
 
 #include "General/Exception.h"
+#include "General/Utility.h"
+
+#include "Containers/Pair.h"
 
 namespace SSTD
 {
@@ -14,74 +17,56 @@ namespace SSTD
   public:
     using AllocType = A<CharType>;
 
-    explicit TString() : m_Allocator{} {}
-
-    TString(SizeType capacity) : m_Allocator()
+    explicit TString(const AllocType& alloc = AllocType()) : m_Allocator{ alloc }
     {
-      if (capacity > SSOSize)
+      SetStateData(StringState::Short, 1);
+    }
+
+    TString(const CharType* str, SizeType size, const AllocType& alloc = AllocType())
+      : m_Allocator{ alloc }
+    {
+      if (size > m_ShortStr.capacity)
       {
-        m_Data.long_string.capacity = capacity;
-        m_Data.long_string.msb_capacity = Bit::MSB(capacity);
-        m_Data.long_string.size = 0;
-        m_Data.long_string.msb_size = ~0;
-        m_Data.long_string.buffer = m_Allocator.Allocate(capacity);
+        SetStateData(StringState::Long);
+        m_LongStr.buffer = TMemCpy<CharType>(m_Allocator.Allocate(size), str, size);
+        m_LongStr.capacity = size;
+        m_LongStr.size = size;
+      }
+      else
+      {
+        SetStateData(StringState::Short, size);
+        TMemCpy<CharType>(m_ShortStr.buffer, str, size);
       }
     }
 
     template <SizeType N>
-    constexpr TString(const CharType(&str)[N]) : TString(str, N - 1) {}
-
-    constexpr TString(const CharType* str, SizeType size)
-    {
-      if (str)
-      {
-        if (size > SSOSize)
-        {
-          size = Grow(size);
-          m_Data.long_string.size = m_Data.long_string.capacity = size;
-          m_Data.long_string.msb_capacity = Bit::MSB(m_Data.long_string.capacity);
-          m_Data.long_string.msb_size = ~m_Data.long_string.msb_capacity;
-          m_Data.long_string.buffer = m_Allocator.Allocate(size);
-          TMemCpy<CharType>(m_Data.long_string.buffer, str, size);
-        }
-        else
-        {
-          m_Data.short_string.size = static_cast<CharType>((SSOSize - size) << 2);
-          TMemCpy<CharType>(m_Data.short_string.buffer, str, size);
-        }
-      }
-    }
+    constexpr TString(const CharType(&str)[N], const AllocType& alloc = AllocType()) :TString(str, N, alloc) {}
 
     TString(const TString& other)
+      : m_Allocator{ other.m_Allocator }
     {
-      if (other.IsShort())
+      const Pair<StringState, uint8> sdata = other.GetStateData();
+      if (sdata.first == StringState::Short)
       {
-        m_Data.short_string.size = other.m_Data.short_string.size;
-        TMemCpy<CharType>(m_Data.short_string.buffer, other.m_Data.short_string.buffer, other.GetShortSize());
+        TMemCpy<uint8>(m_RawStr.buffer, other.m_RawStr.buffer, sizeof(m_RawStr.buffer));
+        m_RawStr.state = other.m_RawStr.state;
       }
-      else
+      else if (sdata.first == StringState::Long)
       {
-        m_Data.long_string.size = other.m_Data.long_string.size;
-        m_Data.long_string.msb_size = other.m_Data.long_string.msb_size;
-
-        m_Data.long_string.capacity = other.m_Data.long_string.capacity;
-        m_Data.long_string.msb_capacity = other.m_Data.long_string.msb_capacity;
-
-        m_Data.long_string.buffer = m_Allocator.Allocate(other.GetLongCapacity());
-        TMemCpy<CharType>(m_Data.long_string.buffer, other.m_Data.long_string.buffer, m_Data.long_string.size);
+        SetStateData(StringState::Long);
+        m_LongStr.buffer = TMemCpy<CharType>(m_Allocator.Allocate(other.m_LongStr.size), other.m_LongStr.buffer, other.m_LongStr.size);
+        m_LongStr.capacity = other.m_LongStr.size;
+        m_LongStr.size = other.m_LongStr.size;
       }
     }
 
-    TString(TString&& other) noexcept : m_Allocator
+    TString(TString&& other) noexcept : m_Allocator{ Move(other.m_Allocator) }
     {
-      Move(other.m_Allocator)
-    }
-    {
-      TMemCpy<uint8>(m_Data.raw_string, other.m_Data.raw_string, sizeof(LongString));
 
-      other.m_Data.long_string.buffer = nullptr;
-      other.m_Data.long_string.size = 0;
-      other.m_Data.long_string.msb_size = 0;
+      TMemCpy<uint8>(m_RawStr.buffer, other.m_RawStr.buffer, sizeof(m_RawStr.buffer));
+      m_RawStr.state = other.m_RawStr.state;
+      SetStateData(StringState::Short, 1);
+      other.m_ShortStr.buffer[0] = NullTerminator;
     }
 
     ~TString()
@@ -95,282 +80,181 @@ namespace SSTD
     }
     bool operator!=(const TString& other)
     {
-      return false;
+      return !(*this == other);
     }
 
     CharType& operator[](SizeType index)
     {
-      return IsShort() ? m_Data.short_string.buffer[index] : m_Data.long_string.buffer[index];
+      return CStr()[index];
     }
     const CharType& operator[](SizeType index) const
     {
-      return IsShort() ? m_Data.short_string.buffer[index] : m_Data.long_string.buffer[index];
+      return CStr()[index];
     }
 
     TString& operator=(const TString& other)
     {
-      if (!IsShort())
-        m_Allocator.Deallocate(m_Data.long_string.buffer);
-
-      if (other.IsShort())
-      {
-        TMemCpy<CharType>(m_Data.short_string.buffer, other.m_Data.short_string.buffer, other.GetShortSize());
-        m_Data.short_string.size = other.m_Data.short_string.size;
-      }
-      else
-      {
-        m_Data.long_string.capacity = other.m_Data.long_string.capacity;
-        m_Data.long_string.msb_capacity = other.m_Data.long_string.msb_capacity;
-        m_Data.long_string.size = other.m_Data.long_string.size;
-        m_Data.long_string.msb_size = other.m_Data.long_string.msb_size;
-        m_Data.long_string.buffer = m_Allocator.Allocate(m_Data.long_string.capacity);
-        TMemCpy<CharType>(m_Data.long_string.buffer, other.m_Data.long_string.buffer, other.GetLongSize());
-      }
       return *this;
     }
 
     TString& operator=(TString&& other) noexcept
     {
-      if (!IsShort())
-        m_Allocator.Deallocate(m_Data.long_string.buffer);
-
-      m_Allocator = Move(other.m_Allocator);
-      TMemCpy<uint8>(m_Data.raw_string, other.m_Data.raw_string, sizeof(LongString));
-
-      other.m_Data.long_string.buffer = nullptr;
-      other.m_Data.long_string.size = 0;
-      other.m_Data.long_string.msb_size = 0;
-
       return *this;
     }
 
-
-    CharType& At(SizeType index) 
+    CharType& At(SizeType index)
     {
-      if (IsShort())
-      {
-        if (index > GetShortSize())
-          throw Exception();
-        return m_Data.short_string.buffer[index];
-      }
-      else
-      {
-        if (index > GetLongSize())
-          throw Exception();
-        return m_Data.long_string.buffer[index];
-      }
+      return operator[](index);
     }
+
     const CharType& At(SizeType index) const
     {
-      if (IsShort())
-      {
-        if (index > GetShortSize())
-          throw Exception();
-        return m_Data.short_string.buffer[index];
-      }
-      else
-      {
-        if (index > GetLongSize())
-          throw Exception();
-        return m_Data.long_string.buffer[index];
-      }
+      return operator[](index);
     }
 
-    CharType& Front() { return IsShort() ? m_Data.short_string.buffer[0] : m_Data.long_string.buffer[0]; }
-    const CharType& Front() const { return IsShort() ? m_Data.short_string.buffer[0] : m_Data.long_string.buffer[0]; }
+    CharType& Front() { }
+    const CharType& Front() const { }
 
-    CharType& Back() { return IsShort() ? m_Data.short_string.buffer[GetShortSize()] : m_Data.long_string.buffer[GetLongSize()]; }
-    const CharType& Back() const { return IsShort() ? m_Data.short_string.buffer[GetShortSize()] : m_Data.long_string.buffer[GetLongSize()]; }
+    CharType& Back() { }
+    const CharType& Back() const { }
 
-    void Resize(SizeType size)
+    void Resize(SizeType size, CharType value = '0')
     {
-      SizeType start_size = 0;
-      if (IsShort())
-      {
-        start_size = GetShortSize();
-        if (size > SSOSize)
-        {
-          auto temp = m_Allocator.Allocate(size);
-          TMemCpy<CharType>(temp, m_Data.short_string.buffer, GetShortSize());
-          m_Data.long_string.buffer = temp;
-          m_Data.long_string.capacity = size;
-          m_Data.long_string.size = size;
-          m_Data.long_string.msb_capacity = Bit::MSB(size);
-          m_Data.long_string.msb_size = ~m_Data.long_string.msb_capacity;
-        }
-        else
-        {
-          m_Data.short_string.size = (SSOSize - size) << 2;
-          m_Data.short_string.buffer[size] = NullTerminator;
-        }
-      }
-      else
-      {
-        start_size = GetLongSize();
-        if (size > GetLongCapacity())
-        {
-          auto temp = m_Allocator.Allocate(size);
-          TMemCpy<CharType>(temp, m_Data.long_string.buffer, GetShortSize());
-          m_Allocator.Deallocate(m_Data.long_string.buffer);
-          m_Data.long_string.buffer = temp;
 
-          m_Data.long_string.capacity = size;
-          m_Data.long_string.msb_capacity = Bit::MSB(size);
-          m_Data.long_string.size = size;
-          m_Data.long_string.msb_size = ~m_Data.long_string.msb_capacity;
-        }
-        else
-        {
-          m_Data.long_string.size = size;
-          m_Data.long_string.msb_size = ~Bit::MSB(size);
-          m_Data.long_string.buffer[size] = NullTerminator;
-        }
-      }
     }
 
     void Reserve(SizeType capacity)
     {
-      if (IsShort())
+      if (capacity <= m_ShortStr.capacity)
+        return;
+
+      CharType* cstr{};
+      SizeType size{};
+
+      const Pair<StringState, uint8> sdata = GetStateData();
+      if (sdata.first == StringState::Short)
       {
-        SizeType c = Grow(SSOSize + capacity);
-        SizeType size = GetShortSize();
-
-        if (c < SSOSize) 
+        if (capacity < sdata.second)
           return;
-        auto temp = m_Allocator.Allocate(c);
-        TMemCpy<CharType>(temp, m_Data.short_string.buffer, size);
-        
-        
-        m_Data.long_string.buffer = temp;
-        m_Data.long_string.capacity = c;
-        m_Data.long_string.msb_capacity = c;
-        m_Data.long_string.size = size;
-        m_Data.long_string.msb_size = ~Bit::MSB(size);
+        cstr = m_ShortStr.buffer;
+        size = sdata.second;
       }
-      else
+      else if (sdata.first == StringState::Long)
       {
-        SizeType c = Grow(GetLongSize() + capacity);
-        SizeType size = GetLongSize();
-        if (c < SSOSize)
+        if (capacity < m_LongStr.size)
           return;
-
-        auto temp = m_Allocator.Allocate(c);
-
-        TMemCpy<CharType>(temp, m_Data.long_string.buffer, GetLongSize());
-        m_Allocator.Deallocate(m_Data.long_string.buffer);
-        m_Data.long_string.size = size;
-
-        m_Data.long_string.buffer = temp;
-        m_Data.long_string.capacity = c;
-        m_Data.long_string.msb_capacity = c;
+        cstr = m_LongStr.buffer;
+        size = m_LongStr.size;
       }
+      SetStateData(StringState::Long);
+      const SizeType new_cap = capacity + size;
+
+      CharType* tmp = m_Allocator.Allocate(new_cap);
+      TMemCpy<CharType>(tmp, cstr, size);
+      m_LongStr.buffer = tmp;
+      m_LongStr.size = size;
+      m_LongStr.capacity = new_cap;
     }
 
     void Minimize()
     {
-      Resize(Size());
+      const Pair<StringState, uint8> sdata = GetStateData();
+      if (sdata.first == StringState::Long)
+      {
+        CharType tmp = m_Allocator.Allocate(m_LongStr.size);
+        TMemCpy<CharType>(tmp, m_LongStr.buffer, m_LongStr.size);
+        m_LongStr.capacity = m_LongStr.size;
+      }
     }
 
     void Clear()
     {
-      if (!IsShort())
-        m_Allocator.Deallocate(m_Data.long_string.buffer);
+      const Pair<StringState, uint8> sdata = GetStateData();
+      if (sdata.first == StringState::Long)
+        m_Allocator.Deallocate(m_LongStr.buffer);
 
-      for (auto i = 0; i < SSOSize; i++)
-        m_Data.raw_string[i] = 0;
-    }
-
-    void Append(const TString& other)
-    {
-      SizeType s = other.Size();
-      Reserve(s);
-      if (IsShort())
-      {
-        TMemCpy<CharType>(m_Data.short_string.buffer + GetShortSize(), other.CStr(), s);
-        m_Data.long_string.size = GetShortSize() + s;
-      }
-      else
-      {
-        TMemCpy<CharType>(m_Data.long_string.buffer + GetLongSize(), other.CStr(), s);
-        m_Data.long_string.size += s;
-      }
+      SetStateData(StringState::Short, 1);
+      m_ShortStr.buffer[0] = NullTerminator;
     }
 
     const CharType* CStr() const
     {
-      return IsShort() ? m_Data.short_string.buffer : m_Data.long_string.buffer;
+      const Pair<StringState, uint8> sdata = GetStateData();
+      if (sdata.first == StringState::Short)
+        return m_ShortStr.buffer;
+      else if (sdata.first == StringState::Long)
+        return m_LongStr.buffer;
     }
 
     SizeType Size() const
     {
-      return IsShort() ? SSOSize - (m_Data.short_string.size >> 2) : Bit::ApplyMSB(static_cast<SizeType> (m_Data.long_string.size), !m_Data.long_string.size);
+      const Pair<StringState, uint8> sdata = GetStateData();
+      if (sdata.first == StringState::Short)
+        return sdata.second - 1;
+      else if (sdata.first == StringState::Long)
+        return m_LongStr.size - 1;
     }
 
     SizeType Capacity() const
     {
-      return IsShort() ? SSOSize : Bit::ApplyMSB(static_cast<SizeType> (m_Data.long_string.capacity), m_Data.long_string.msb_capacity);
+      const Pair<StringState, uint8> sdata = GetStateData();
+      if (sdata.first == StringState::Short)
+        return m_ShortStr.capacity;
+      else if (sdata.first == StringState::Long)
+        return m_LongStr.capacity - 1;
     }
 
     bool IsEmpty() const
     {
-      return Size() == 0;
+      return true;
     }
 
   private:
 
-    SizeType GetShortSize() const
+    enum StringState : uint8
     {
-      return static_cast<SizeType>(SSOSize) - (m_Data.short_string.size >> 2);
-    }
-
-    SizeType GetLongSize() const
-    {
-      return Bit::ApplyMSB(static_cast<SizeType> (m_Data.long_string.size), !m_Data.long_string.size);
-    }
-
-     SizeType GetLongCapacity() const
-    {
-      return Bit::ApplyMSB(static_cast<SizeType> (m_Data.long_string.capacity), m_Data.long_string.msb_capacity);
-    }
-
-     bool IsShort() const
-    {
-      return !m_Data.long_string.msb_capacity & !m_Data.long_string.msb_size;
-    }
-
-    constexpr SizeType Grow(SizeType s) const
-    {
-      return static_cast<SizeType>(s * 1.6);
-    }
-
-    struct LongString
-    {
-      CharType* buffer;
-      SizeType capacity : sizeof(SizeType) * 8 - 1;
-      SizeType msb_capacity : 1;
-      SizeType size : sizeof(SizeType) * 8 - 1;
-      SizeType msb_size : 1;
+      Short = 0x00,
+      Long = 0x01,
     };
+    static constexpr size_t StateMask = 0b11; // least two bits set to one
 
-    static const size_t SSOSize = (sizeof(LongString) / sizeof(CharType));
-
-    struct ShortString
+    Pair<StringState, uint8> GetStateData() const
     {
-      CharType buffer[SSOSize - 1];
-      CharType size;
-    };
-
-    union Data
-    {
-      LongString long_string;
-      ShortString short_string;
-      uint8 raw_string[sizeof(LongString)] = { 0 };
+      return { static_cast<StringState>(m_RawStr.state & StateMask), static_cast<uint8>(m_RawStr.state >> 2) };
     }
-    m_Data;
+
+    void SetStateData(const StringState state, uint8 data = 0)
+    {
+      m_RawStr.state = (data << 2) | state;
+    }
+
+    union
+    {
+      struct LongStr
+      {
+        CharType* buffer;
+        SizeType size;
+        SizeType capacity;
+      private:
+        uint8 state;
+      } m_LongStr;
+
+      struct RawStr
+      {
+        uint8 buffer[sizeof(LongStr) - 1]{0};
+        uint8 state{ 0 };
+      } m_RawStr{};
+
+      struct ShortStr
+      {
+        static constexpr size_t capacity = (sizeof(LongStr) - sizeof(StringState)) / sizeof(CharType);
+        CharType buffer[capacity];
+      } m_ShortStr;
+    };
 
     [[msvc::no_unique_address]] AllocType m_Allocator;	//TODO: this is MSVC specific, we may need to work with different compilers at some point
   };
 
-  using String   = TString<char, '\0'>;
+  using String = TString<char, '\0'>;
   using WString = TString<wchar_t, L'\0'>;
 }
